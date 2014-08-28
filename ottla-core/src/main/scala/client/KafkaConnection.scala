725c9a9a3d3f9170f7cac9debe9f6a12398b42e5
Anton Karamanov <ataraxer@gmail.com>
2014-08-28 13:08:41 +0400

    Move kafka connection actor to `client` directory

--- ottla-core/src/main/scala/client/KafkaConnection.scala
+++ ottla-core/src/main/scala/client/KafkaConnection.scala
@@ -1,10 +1,14 @@
-package com.ataraxer.ottla.kafka
+package com.ataraxer.ottla
+package client
 
 import akka.actor._
 import akka.actor.Actor.Receive
 import akka.io.{ IO, Tcp }
+import akka.util.ByteString
 
 import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+
 
 
 class KafkaConnection(remote: InetSocketAddress, listener: ActorRef)
@@ -17,14 +21,14 @@ class KafkaConnection(remote: InetSocketAddress, listener: ActorRef)
 
 
   def receive = {
-    case CommandFailed(_: Connect) => // TODO
-
     case cmd @ Connected(remote, local) => {
       listener ! cmd
       val connection = sender
       connection ! Register(self)
       context.become(active(connection))
     }
+
+    case CommandFailed(_: Connect) => // TODO
   }
 
 
@@ -32,14 +36,15 @@ class KafkaConnection(remote: InetSocketAddress, listener: ActorRef)
     case Received(data) => {
       // process response from Kafka
       // and send back to requester
+      listener ! data
     }
 
-    case writeRequest: Write => {
-      connection ! writeRequest
+    case request: Write => {
+      connection ! request
     }
 
     case CommandFailed(_: Write) => // TODO
-    case _: ConnectionClosed     => // TODO
+    case c: ConnectionClosed     => // TODO
   }
 }
 
