8bd816e8a7a9a2c3dc3ef2babe48eb22530f3878
Anton Karamanov <ataraxer@gmail.com>
2014-09-29 23:03:21 +0400

    Add `balanceReplicas`

--- ottla-core/src/main/scala/Kafka.scala
+++ ottla-core/src/main/scala/Kafka.scala
@@ -1,8 +1,55 @@
 package com.ataraxer.ottla
 
+import scala.collection.mutable
+import scala.util.Random
+
 
 object Kafka {
   case class Partition(topic: String, id: Int)
+
+
+  def balanceReplicas(
+    brokerList: Seq[Int],
+    partitionCount: Int,
+    replicationFactor: Int) =
+  {
+    val result = mutable.HashMap.empty[Int, List[Int]]
+
+    val brokerCount = brokerList.size
+
+    val startIndex = Random.nextInt(brokerCount)
+    var nextReplicaShift = Random.nextInt(brokerCount)
+
+    for (partitionId  <- 0 until partitionCount) {
+      if (partitionId > 0 && (partitionId % brokerCount == 0)) {
+        nextReplicaShift += 1
+      }
+
+      val firstReplicaIndex = (partitionId + startIndex) % brokerCount
+
+      val replicaList = for {
+        j <- 0 until replicationFactor - 1
+      } yield {
+        brokerList(
+          replicaIndex(firstReplicaIndex, nextReplicaShift, j, brokerCount))
+      }
+
+      result.put(partitionId, brokerList(firstReplicaIndex) :: replicaList.toList)
+    }
+
+    result.toMap
+  }
+
+
+  private def replicaIndex(
+    firstReplicaIndex: Int,
+    secondReplicaShift: Int,
+    replicaIndex: Int,
+    nBrokers: Int): Int =
+  {
+    val shift = 1 + (secondReplicaShift + replicaIndex) % (nBrokers - 1)
+    (firstReplicaIndex + shift) % nBrokers
+  }
 }
 
 
